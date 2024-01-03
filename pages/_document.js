709cb85276f1ed9ea4f6e78b26f5fe6955fa6e29
Kaide <kaidewu@protonmail.com>
2024-01-03 12:11:38 +0100

    Adding analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 import { ColorModeScript } from '@chakra-ui/react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 import theme from '../lib/theme'
+import { Analytics } from '@vercel/analytics/react'
 
 export default class Document extends NextDocument {
   render() {
@@ -12,6 +13,7 @@ export default class Document extends NextDocument {
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
+          <Analytics />
         </body>
       </Html>
     )
